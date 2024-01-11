df0cf23f04c8a1f9b25c592d7769533080a0790d
unknown <nami0879@naver.com>
2024-01-12 08:31:12 +0900

    요격시스템

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.programmers.MissileDefenseSystem;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] targets = {{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}};
+        int answer = MissileDefenseSystem.solve(targets);
+        System.out.println(answer);
     }
 }
