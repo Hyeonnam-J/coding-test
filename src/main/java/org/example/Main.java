562ebe5e45217a5b5c022b94f62bc85d57bc3bd7
unknown <nami0879@naver.com>
2024-03-10 21:06:25 +0900

    디펜스 게임 준비

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import org.example.programmers.요격시스템;
+import org.example.programmers.디펜스_게임;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println( 요격시스템.풀이() );
+        System.out.println( 디펜스_게임.풀이() );
     }
 }
