013964223f3f8b0c51ab1d8513e8346a932ceea3
unknown <nami0879@naver.com>
2024-04-29 20:07:32 +0900

    멀리 뛰기

--- src/main/java/org/example/programmers/멀리_뛰기.java
+++ src/main/java/org/example/programmers/멀리_뛰기.java
@@ -13,8 +13,8 @@ public class 멀리_뛰기 {
         (2칸, 2칸)
         의 5가지 방법으로 맨 끝 칸에 도달할 수 있습니다.
         멀리뛰기에 사용될 칸의 수 n이 주어질 때, 효진이가 끝에 도달하는 방법이 몇 가지인지 알아내,
-        -여기에 1234567를 나눈 나머지를 리턴하는 함수,- (?)
-        solution을 완성하세요.
+        여기에 1234567를 나눈 나머지를 리턴하는 함수 solution을 완성하세요.
+
         예를 들어 4가 입력된다면, 5를 return하면 됩니다.
 
         <제한 사항>
@@ -29,23 +29,21 @@ public class 멀리_뛰기 {
      */
 
     public static long 풀이(){
-        int n = 4;
+        int n = 1;
+        return countWays(n);
+    }
 
-        int twoBlockMaxNo = n / 2;
+    public static long countWays(int n) {
+        if(n == 1) return 1L;
 
-        long answer = 0;
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-        for(int twoBlockNo = twoBlockMaxNo; twoBlockNo > -1; twoBlockNo--){
-            int oneBlockNo = n - (twoBlockNo * 2);
-
-            if(oneBlockNo == 0 || twoBlockNo == 0) {
-                answer++;
-                continue;
-            }
-
-            int blockNo = twoBlockNo + oneBlockNo;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
 
-        return answer;
+        return dp[n];
     }
 }
